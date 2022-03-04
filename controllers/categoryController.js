af30b24c550a4765cfbe65f15406a2b3b9c860ac
Thales Ruano <thalesoruano@gmail.com>
2022-03-04 19:52:54 -0300

    feat create categories CRUD

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,13 +1,41 @@
-
+import db  from '../services/db.js';
 
 async function getCategories(req, res)
 {
-
+    try {
+        const result = await db.query(`SELECT * FROM categories`);
+        res.send(result.rows);
+    }
+    catch(err)
+    {
+        res.status(400).send('erro no banco de dados: '+err);
+    }
 }
 
 async function postCategory(req, res)
 {
+    try {
+        const { name } = res.locals.postData;
+       
+        const result = await db.query("SELECT * FROM categories WHERE name=$1", [name]);
 
+        if( result?.rowCount > 0)
+        {
+            res.status(401).send("Essa categoria já está cadastrada!");
+            return;
+        }
+        
+        await db.query(`
+        INSERT INTO
+        categories (name)
+        VALUES ($1)
+        `, [name]);
+        res.sendStatus(200);
+    }
+    catch(err)
+    {
+        res.status(500).send('erro no banco de dados: '+err);
+    }
 }
 
 export {
